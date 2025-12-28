5f75bd2b760b0b39e40e7d9805f21110bdf12cd1
tpganime <139879328+tpganime@users.noreply.github.com>
2025-12-28 13:51:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,26 @@
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const { distubeOptions } = require('./src/config/config.js');
-const fs = require('fs');
-const path = require('path');
-const PlayerManager = require('./src/player/PlayerManager.js');
-const { printWatermark } = require('./src/config/type.js');
-const intents = [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMembers,
-  GatewayIntentBits.GuildVoiceStates,
-  GatewayIntentBits.GuildMessages
-];
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+const { DisTube } = require("distube");
+const { SpotifyPlugin } = require("@distube/spotify");
+const config = require("./config.json");
 
-const client = new Client({ intents });
-printWatermark();
-client.commands = new Collection();
-client.playerManager = new PlayerManager(client, distubeOptions);
-client.playerManager.distube.setMaxListeners(20);
-const commandsPath = path.join(__dirname, './src/commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-  const command = require(path.join(commandsPath, file));
-  client.commands.set(command.data.name, command);
-}
-
-
-const eventsPath = path.join(__dirname, './src/events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args, client));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args, client));
-  }
-}
-
-
-const express = require("express");
-const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-    const imagePath = path.join(__dirname, 'index.html');
-    res.sendFile(imagePath);
-});
-app.listen(port, () => {
-    console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
+client.distube = new DisTube(client, {
+  emitNewSongOnly: true,
+  plugins: [new SpotifyPlugin()]
+});
 
-client.login(process.env.TOKEN);
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  client.user.setActivity(config.botName);
+});
+
+client.login(process.env.DISCORD_TOKEN);
