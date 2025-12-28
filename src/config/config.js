eb6642e387b187cfef8c5d04fc0dec0de99ac986
tpganime <139879328+tpganime@users.noreply.github.com>
2025-12-28 13:52:20 +0530

    Refactor config.js to update bot settings
    Removed dotenv configuration and cookie settings, added bot configuration.

--- src/config/config.js
+++ src/config/config.js
@@ -1,29 +1,5 @@
-require('dotenv').config();
-
-
-module.exports = {
-    token: '',
-    cookies:{
-        domain: ".youtube.com",
-        expirationDate: 1757764090.235338,
-        hostOnly: false,
-        httpOnly: false,
-        name: "__Secure-1PAPISID",
-        path: "/",
-        sameSite: "unspecified",
-        secure: true,
-        session: false,
-        storeId: "0",
-        value: "1WEHRrYUBttCn9ih/ACbOwdBzceu9lwe-x",
-        id: 1
-    },
-    
-    distubeOptions: {
-        emitAddListWhenCreatingQueue: true,
-        emitAddSongWhenCreatingQueue: false,
-        emitNewSongOnly: true,
-        joinNewVoiceChannel: true,
-        nsfw: true,
-        savePreviousSongs: true,
-    }
-};
+{
+  "botName": "─・𒀛TPG ｜MUSIC",
+  "prefix": "!",
+  "embedColor": "#ff0000"
+}
